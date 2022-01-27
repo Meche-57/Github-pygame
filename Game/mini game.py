7e984013d1bd8be64dae2c20d8e7614fb3f4b4d1
Meche-57 <93935558+Meche-57@users.noreply.github.com>
2022-01-27 11:56:07 +0000

    Updated on coins
    *coins reappear once the game restarted

--- Game/mini game.py
+++ Game/mini game.py
@@ -1,5 +1,16 @@
 import pygame 
 import sys
+import time
+
+
+#window 
+
+pygame.init()
+clock = pygame.time.Clock() 
+window_width = 600
+window_height = 330
+window = pygame.display.set_mode((window_width,window_height))
+pygame.display.set_caption('Runner')
 
 def game_timer():
     time = int(pygame.time.get_ticks()/1000) - start_time
@@ -7,14 +18,6 @@ def game_timer():
     time_rect = time_surf.get_rect(topleft =(525,10))
     window.blit(time_surf,time_rect)
 
-
-#window 
-pygame.init()
-clock = pygame.time.Clock() 
-window_width = 600
-window_height = 330
-window = pygame.display.set_mode((window_width,window_height))
-pygame.display.set_caption('Runner')
 
 start_time = 0 
 
@@ -27,7 +30,9 @@ font3 = pygame.font.Font(None, 70)
 font4 = pygame.font.Font(None, 40)
 
 
-#surface
+
+
+#surface                                                                             
 score = font.render('score:',False,'white')
 timer = font.render('Time:',False,'white')
 Over = font2.render('Game Over',False,'white')     # text information , false, colour
@@ -35,38 +40,54 @@ play_again = font3.render('Play Again?',False,'white')
 no= font4.render('No',False,'dark blue')
 yes = font4.render('Yes(space for now)',False,'dark green')
 fire = pygame.image.load('fire.png').convert_alpha()#removing alpha values and making the game run smoother as its easier for pygame to manage 
-player = pygame.image.load('player_walk1.png').convert_alpha()
+player = pygame.image.load('player_1.png').convert_alpha()
 cave = pygame.image.load('cave.png').convert()
 dirt = pygame.image.load('dirt2.png').convert()
-coin = pygame.image.load('coin.png').convert()
-coin_e = pygame.transform.scale(coin,(40,40))
-fire_e = pygame.transform.scale(fire,(65,65)) #lenghth and width 
 
-# not needed anymore as we are using rect to simplify 
+
+
+
+
+# not needed anymore as we are using rect to simplify
 #fire_x_pos = 525
 #fire_y_pos = 240
-#player_x_pos = 200
-#player_y_pos = 240
+player_x_pos = 10
+player_y_pos = 300
+player_width = 37
+player_height = 75
 
 # rectangle 
 
-player_rect = player.get_rect(midbottom = (20,0)) # adding rec around the surface # coordinate of rec too
-fire_rect = fire.get_rect(bottomright= (660,350))
+player_rect = player.get_rect(bottomleft= (0,0))# adding rec around the surface # coordinate of rec too
+fire_rect = fire.get_rect(bottomright= (0,280))
 score_rect = score.get_rect(topleft = (10,10))
 timer_rect = timer.get_rect(topleft=(400,10))
 over_rect = Over.get_rect(topleft = (105,0))
 play_again_rect = play_again.get_rect(topleft =(160,90))
 yes_rect = yes.get_rect(topleft=(270,180))
 no_rect = no.get_rect(topleft=(272,250))
-coin_rect = coin.get_rect(topleft = (100,140))
+
 player_gravity =  0
 
+
 run = True
-menu = False
+game_over = False
 game_start = True
 
 
 
+coin_image = pygame.image.load('coin.png').convert()
+coin_e = pygame.transform.scale(coin_image,(40,40))
+
+coins_rect = [
+    pygame.Rect(150,100,60,60),
+    pygame.Rect(50,100,60,60),
+    pygame.Rect(200,100,60,60)
+]
+
+
+points = 0     
+   
 
 while run:
 
@@ -76,15 +97,31 @@ while run:
                 pygame.quit()
                 sys.exit()
         
-        if event.type == pygame.QUIT:
+        elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         
+        
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            print("RESET")
             fire_rect.left = 600
             player_rect.left = 0
+            
             game_start = True
+            
             start_time = int(pygame.time.get_ticks()/ 1000)
+            points = 0 
+            coins_rect = [
+            pygame.Rect(150,100,60,60),
+            pygame.Rect(50,100,60,60),
+            pygame.Rect(200,100,60,60)
+            ]
+
+
+            for c in coins_rect: # for ever column / row in the list
+                window.blit(coin_e,(c[0],c[1]))
+    
+            
 
        
     
@@ -103,19 +140,34 @@ while run:
             
     if game_start:
         window.blit(cave,(0,0))
-        window.blit(dirt,(0,280))
+        window.blit(dirt,(0,290))
         window.blit(score,score_rect)
         window.blit(timer,timer_rect)
         window.blit(player,player_rect)
-        window.blit(coin_e,coin_rect)
+        
+        
         game_timer()
-       
+        
+        for c in coins_rect: # for ever column / row in the list
+            window.blit(coin_e,(c[0],c[1]))
+
+            if c.colliderect(player_rect):
+                coins_rect.remove(c)
+                
+                
+                points = points + 1
+        
+        points_surf = font.render(f'{points}', False , 'white')
+        points_rect = points_surf.get_rect(topleft = (100, 10))
+        window.blit(points_surf,points_rect)
+            
+              
         
         # fire 
         fire_rect.left -= 2
         if fire_rect.left <= -100:
             fire_rect.left = 600 # is the screen
-        window.blit(fire_e,fire_rect)
+        window.blit(fire,fire_rect)
         
         
         player_gravity += 1  
@@ -128,6 +180,8 @@ while run:
         
         if fire_rect.colliderect(player_rect):
             game_start = False # game stops
+        
+        
     
     
     else:
@@ -163,11 +217,15 @@ while run:
 
 
 # WHAT TO DO NEXT:
-# modify size of fire_rect, probably modisize the players rect too
-# add image of coin and if player collide with coin score_count = +1 
+# modify size of fire_rect, probably modisize the players rect too ---> update fire ball ------------DONE 
+# add image of coin and if player collide with coin score_count ___________________________DONE
+# reset function 
+
+
 # learn how to do sprites to join the rect and surface into one class 
 # spawn multiple fires using sprite 
 #  work on animation for player using sprite
 
 
-#updating of player .. player needs to touch the ground and the move again..otherwise it moves in the air in arrow pressed
+#updating of player
+# fix problem of resetting while playing game when spacebar is pressed 
